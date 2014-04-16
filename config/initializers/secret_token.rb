301568e299e8ca708a3141d55d2c1a134e066d13
fantini <fantini@fantini-VirtualBox.(none)>
2014-04-15 22:50:08 -0300

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nosotrosayudamos::Application.config.secret_key_base = '14fcf5b03fbd874e3a6b2c51a6a51566cc185ce69659dfbcfa45c355fbf32b28973ee96fcc5b1ba06e1e5f7b5465bbd6ee1d8f6dd61510f8b4bd4934e9da19b7'
